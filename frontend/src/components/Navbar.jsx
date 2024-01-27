25de59f086ca5632b39b366a70ce1a34be179dfb
Aiman Fadillah <mlakece914@gmail.com>
2024-01-27 17:53:05 +0700

    navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom"
+
+export default function Navbar ({children}) {
+    return <>
+    <nav className="navbar navbar-expand-md ">
+        <div className="container-fluid px-4">
+            <a className="navbar-brand fs-2" href="#">Web</a>
+            <button type="button" className="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="offcanvas offcanvas-end" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div className="offcanvas-header">
+                    <h5 className="offcanvas-title fs-2" id="offcanvasNavbarLabel">Menu</h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <ul className="p-3 navbar-nav me-auto d-flex justify-content-end gap-2" style={{ width:"100%" }}>
+                    <NavLink text={"Beranda"} to={"/"}/>
+                    <NavLink text={"Project"} to={"/"}/>
+                    <NavLink text={"Keahlian"} to={"/"}/>
+                    <NavLink text={"Kontak"} to={"/"}/>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    {children}
+    </>
+}
+
+function NavLink ({active,text,to}){
+    return <li className={`nav-item ${active ? "bg-primary rounded fw-bold" : ""}`} >
+        <Link className={`nav-link px-2 ${active ? "text-light" : ""}`} to={to}>{text}</Link>
+    </li>
+}
--- frontend/src/pages/Beranda.jsx
+++ frontend/src/pages/Beranda.jsx
@@ -1,11 +1,15 @@
 import { useContext } from "react"
 import Sidebar from "../components/Sidebar"
 import DataContext from "../variabels/Context"
+import Navbar from "../components/Navbar";
+import { Container } from "../components/Grid";
 
 export default function Beranda () {
     const {user,setUser,userFunction} = useContext(DataContext);
 
-    return <Sidebar>
-        <h1>Hai {user.nama} <button className="btn btn-danger" onClick={() => userFunction.remove(setUser)} >logout</button> </h1>
-    </Sidebar>
+    return <Navbar>
+        <Container>
+            <h1>Hai {user.nama} <button className="btn btn-danger" onClick={() => userFunction.remove(setUser)} >logout</button> </h1>
+        </Container>
+    </Navbar>
 }
