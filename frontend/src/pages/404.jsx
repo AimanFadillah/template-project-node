a02a533b451ad472db931744f5cb309567a55499
Aiman Fadillah <mlakece914@gmail.com>
2024-01-30 18:25:30 +0700

    memperbagus tampiIan 404

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -1,3 +1,8 @@
+import { Container } from "../components/Grid";
+
 export default function Page404 () {
-    return <h1 className="mt-5 text-center" >404</h1>
+    // return <h1 className="mt-5 text-center" >404</h1>
+    return <div className="container d-flex align-items-center" style={{ flexDirection:"column" }} >
+        <img src="/img/404.png" className="img-fluid img-404" alt="Foto 404" ></img>
+    </div>
 }
