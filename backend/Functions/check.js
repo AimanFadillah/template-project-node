a986c1f0c0e51167337ec0715cbeea97dd7e0233
Aiman Fadillah <mlakece914@gmail.com>
2024-01-30 17:18:43 +0700

    function image

--- backend/Functions/check.js
+++ backend/Functions/check.js
@@ -1,7 +1,5 @@
 import { validationResult } from "express-validator";
-import { v4 } from "uuid"; 
 import jwt from "jsonwebtoken";
-import path from "path";
 
 export function checkLogin (req,res,next) {
     const token = req.cookies.login;
@@ -20,29 +18,3 @@ export function checkValidate (req,res,next) {
     return next()
 }
 
-export function checkImage (req,file){
-    let foto;
-
-    try{
-        foto = req.files[file]  
-    }catch (e) {
-        return {msg:"Tidak ada gambar"};
-    }
-
-    const allowedType = [".jpg",".jpeg",".png"]
-    const size = mb(10);
-    const ext = path .extname(foto.name);
-    const fileSize = foto.data.length;
-    const fileName = v4() + ext;
-    const url = `${req.protocol}://${req.get("host")}/img/${fileName}`
-
-    if(!allowedType.includes(ext.toLocaleLowerCase())) return {msg:"Yang diupload bukan gambar"}
-    if(fileSize > size) return {msg:"Gambar maksimal 5 mb"}
-
-    foto.mv(`./Public/img/${fileName}/`)
-
-    return {
-        msg:"success",
-        data:url,
-    }
-}
---
+++ backend/Functions/image.js
@@ -0,0 +1,38 @@
+import { v4 } from "uuid"; 
+import path from "path";
+
+export function uploadImage (req,file){
+    let foto;
+
+    try{
+        foto = req.files[file]  
+    }catch (e) {
+        return {msg:"Tidak ada gambar"};
+    }
+
+    const allowedType = [".jpg",".jpeg",".png"]
+    const size = mb(10);
+    const ext = path .extname(foto.name);
+    const fileSize = foto.data.length;
+    const fileName = v4() + ext;
+    const url = `${req.protocol}://${req.get("host")}/img/${fileName}`
+
+    if(!allowedType.includes(ext.toLocaleLowerCase())) return {msg:"Yang diupload bukan gambar"}
+    if(fileSize > size) return {msg:"Gambar maksimal 5 mb"}
+
+    foto.mv(`./Public/img/${fileName}/`)
+
+    return {
+        msg:"success",
+        data:url,
+    }
+}
+
+export function destroyImage (url) {
+    try{
+        fs.unlinkSync(`./Public/img/${(url.split("/"))[url.split("/").length - 1]}`)
+        return {msg:"success"};
+    }catch(e){
+        return {msg:e};
+    }
+}
