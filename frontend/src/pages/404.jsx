834fcc1cd01f1fcd840fdbcb48d66a154a8df43c
AimanFadillah <mlakece914@gmail.com>
2025-01-29 10:37:39 +0700

    [fix] bug grid

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -1,5 +1,3 @@
-import { Container } from "../components/Grid";
-
 export default function Page404 () {
     // return <h1 className="mt-5 text-center" >404</h1>
     return <div className="container d-flex align-items-center" style={{ flexDirection:"column" }} >
